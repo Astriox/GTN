e9ca191bc5c15e69942cbb19a45958e1bd8be4e9
ᴀsᴛʀɪᴏx <71863236+Astriox@users.noreply.github.com>
2023-04-03 15:12:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DKBOTx/TovinoBotV3.git /TovinoBotV3
+  git clone https://github.com/Astriox/GTN.git /GTN
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TovinoBotV3
+  git clone $UPSTREAM_REPO /GTN
 fi
-cd /TovinoBotV3
+cd /GTN
 pip3 install -U -r requirements.txt
 echo "Starting 𝘋𝘒_𝘉𝘖𝘛𝘹...."
 python3 bot.py
